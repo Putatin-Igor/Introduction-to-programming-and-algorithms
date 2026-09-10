51339e52e4154434fb5de82b65dae9190cc8b2ef
Путятин Игорь <waltergunz5@gmail.com>
2026-09-10 22:38:02 +0300

    Решение 1 задания

---
+++ Lab1/Task1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<clocale>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RU");
+	double radius;
+	double length;
+	while (true) { //Цикл для проверки правильности введенного значения
+		cout << "Введите радиус: ";
+		cin >> radius;
+		if (cin.fail() || radius<=0) { //Проверка правильности введенного значения(должно быть введено значение типа double и оно должно быть больше 0
+			cout << "Ошибка ввода! Введите числовое значение больше 0.\n";
+			cin.clear();
+			cin.ignore(1000, '\n');
+		}
+		else {
+			break;
+		}
+	}
+	length = 2 * 3.14 * radius; //Ищем длину
+	cout << length;
+	return 0;
+}
